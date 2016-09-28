ddb2c49985d6668464f53a2c555860c03e7faa2b
Matthew VanTassel <matthew@vantassel.ca>
2016-09-28 08:29:37 -0300

    More logical grouping of torrent data by state - Use promises and setTimeout to ensure there is no collision due to latency

--- deluge2influx.js
+++ deluge2influx.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const influx = require('influx');
-const request = require('request');
+const request = require('request-promise');
 
 const checkInterval = process.env.UPDATE_INTERVAL_MS || 1000 * 30;
 
@@ -19,71 +19,86 @@ const delugeConfig = {
     password: process.env.DELUGE_PASSWORD || 'deluge'
 };
 
-const delugeOptions = {
-    url: `${delugeConfig.protocol}://${delugeConfig.host}:${delugeConfig.port}/json`
-};
-
 let authCookie;
 
-function authDeluge(callback) {
-    return request({
-        method: 'POST',
-        url: delugeOptions.url,
-        json: true,
-        gzip: true,
+let delugeRequestObj = {
+    method: 'POST',
+    url: `${delugeConfig.protocol}://${delugeConfig.host}:${delugeConfig.port}/json`,
+    json: true,
+    gzip: true,
+    resolveWithFullResponse: true
+};
+
+function authDeluge() {
+    return request(Object.assign(delugeRequestObj, {
         body: {
             method: 'auth.login',
             params: [delugeConfig.password],
             id: 1
         }
-    }, callback);
+    }));
 }
 
-function getDelugeTorrents(callback) {
-    return request({
-        method: 'POST',
-        url: delugeOptions.url,
+function getDelugeTorrents() {
+    return request(Object.assign(delugeRequestObj, {
         headers: {
             'Cookie': authCookie
         },
-        json: true,
-        gzip: true,
         body: {
             method: 'web.update_ui',
             params: [
-                ["name", "total_size", "state", "progress", "ratio", "time_added", "tracker_host", "total_done", "total_uploaded"],
+                ['name', 'total_size', 'state', 'progress', 'ratio', 'time_added', 'tracker_host', 'total_done', 'total_uploaded'],
                 []
             ],
             id: 1
         }
-    }, callback);
+    }));
 }
 
 function writeToInflux(seriesName, values, tags, callback) {
     return influxClient.writePoint(seriesName, values, tags, callback);
 }
 
-function onAuthDeluge(error, response, body) {
+function onAuthDeluge(response) {
     authCookie = JSON.stringify(response.headers['set-cookie']).slice(2,50);
 }
 
-function onGetDelugeTorrents(error, response, body) {
-    var torrents = body.result.torrents;
-    var torrentKeys = Object.keys(torrents);
+function onGetDelugeTorrents(response) {
+    let torrents = response.body.result.torrents;
+    let torrentKeys = Object.keys(torrents);
 
-    torrentKeys.forEach(function(torrentKey) {
+    let torrentsByState = {
+        'Downloading': {
+            count: 0,
+            downloaded: 0,
+            uploaded: 0
+        },
+        'Seeding': {
+            count: 0,
+            downloaded: 0,
+            uploaded: 0
+        }
+    };
+
+    torrentKeys.forEach(torrentKey => {
+        let torrent = torrents[torrentKey];
+
+        torrentsByState[torrent.state].count++;
+        torrentsByState[torrent.state].downloaded += torrent.total_done;
+        torrentsByState[torrent.state].uploaded += torrent.total_uploaded;
+
         let value = {
-            added: torrents[torrentKey].time_added,
-            name: torrents[torrentKey].name,
-            progress: torrents[torrentKey].progress,
-            size: torrents[torrentKey].total_size,
-            ratio: torrents[torrentKey].ratio,
-            downloaded: torrents[torrentKey].total_done,
-            uploaded: torrents[torrentKey].total_uploaded
+            name: torrent.name,
+            progress: torrent.progress,
+            size: torrent.total_size,
+            ratio: torrent.ratio,
+            downloaded: torrent.total_done,
+            uploaded: torrent.total_uploaded
         };
+
         let tags = {
-            state: torrents[torrentKey].state,
-            tracker: torrents[torrentKey].tracker_host
+            state: torrent.state,
+            tracker: torrent.tracker_host
         };
 
         writeToInflux('torrent', value, tags, function() {
@@ -91,18 +106,37 @@ function onGetDelugeTorrents(error, response, body) {
         });
     });
 
-    writeToInflux('torrents', {
-        count: torrentKeys.length
-    }, null, function() {
-        console.dir(`wrote ${torrentKeys.length} torrents data to influx: ${new Date()}`);
+    Object.keys(torrentsByState).forEach(state => {
+        let value = {
+            count: torrentsByState[state].count,
+            downloaded: torrentsByState[state].downloaded,
+            uploaded: torrentsByState[state].uploaded
+        };
+
+        let tags = {
+            state: state
+        };
+
+        writeToInflux('torrents', value, tags, function() {
+            console.dir(`wrote ${tags.state} torrent data to influx: ${new Date()}`);
+        });
     });
+
+}
+
+function restart() {
+    // Every {checkInterval} seconds
+    setTimeout(getAllTheMetrics, checkInterval);
 }
 
 function getAllTheMetrics() {
     if (!authCookie) {
-        authDeluge(onAuthDeluge);
+        authDeluge().then(onAuthDeluge)
+            .then(getDelugeTorrents).then(onGetDelugeTorrents)
+            .finally(restart);
     } else {
-        getDelugeTorrents(onGetDelugeTorrents);
+        getDelugeTorrents().then(onGetDelugeTorrents)
+            .finally(restart);
     }
 }
 
@@ -111,6 +145,4 @@ setInterval(function() {
     authDeluge(onAuthDeluge);
 }, 1000 * 60 * 60);
 
-// Every {checkInterval} seconds
 getAllTheMetrics();
-setInterval(getAllTheMetrics, checkInterval);
